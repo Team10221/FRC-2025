170b8ce3de4f6747036d89f3b9791c3a447ed35f
q13x <84165981+WorldEditAxe@users.noreply.github.com>
2025-03-31 19:06:26 -0700

    no sim

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -1,6 +1,5 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.simulation.ElevatorSim;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.lib.motor.Motor;
 import frc.robot.Constants.ElevatorConstants;
