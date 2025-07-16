e0d7db422d9c52db4d7476db911c2b21dd0213f6
Sebastian Dowell <KN6BDL@protonmail.com>
2025-07-16 14:22:22 -0700

    test algae manipulator pivot

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,24 +3,32 @@ package frc.robot;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.PS4Controller;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.subsystems.AlgaeManipulator;
+import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.SwerveSubsystem;
 import frc.robot.subsystems.VisionSubsystem;
+import frc.robot.Constants.AlgaeManipConstants.AlgaeManipAngleState;
+import frc.robot.Constants.AlgaeManipConstants.AlgaeManipState;
 import frc.robot.Constants.ControllerConstants;
+import frc.robot.Constants.ElevatorConstants.ElevatorState;
 import frc.robot.Constants.RobotState;
 import frc.robot.auto.AutoModeSelector;
 
 public class RobotContainer {
   public SwerveSubsystem swerve;
-  public VisionSubsystem vision;
+  // public VisionSubsystem vision;
+  public Elevator elevator;
+  public AlgaeManipulator algaeManipulator;
 
-  public PS4Controller primary;
+  public XboxController primary;
 
   private RobotState currentState;
-  private AutoModeSelector autoModeSelector;
+  // private AutoModeSelector autoModeSelector;
 
   public RobotContainer() {
     try { this.swerve = new SwerveSubsystem(); }
@@ -29,13 +37,16 @@ public class RobotContainer {
       e.printStackTrace();
       System.exit(1);
     }
-    this.vision = new VisionSubsystem(swerve);
+    // this.vision = new VisionSubsystem(swerve);
     // this.coralManipulator = new CoralManipulator();
+    this.elevator = new Elevator();
+    this.algaeManipulator = new AlgaeManipulator();
 
-    this.primary = new PS4Controller(ControllerConstants.PRIMARY_PORT);
+    // this.primary = new PS4Controller(ControllerConstants.PRIMARY_PORT);
+    this.primary = new XboxController(ControllerConstants.PRIMARY_PORT);
 
     this.currentState = RobotState.NORMAL;
-    this.autoModeSelector = new AutoModeSelector(this);
+    // this.autoModeSelector = new AutoModeSelector(this);
     
     configureBindings();
 
@@ -72,13 +83,34 @@ public class RobotContainer {
 
   private void configureBindings() {
     // PRIMARY CONTROLS
+    /* 
     new JoystickButton(primary, PS4Controller.Button.kCircle.value)
       .whileTrue(new RunCommand(() -> swerve.lockPose()));
     new JoystickButton(primary, PS4Controller.Button.kTriangle.value)
-      .onTrue(new InstantCommand(() -> swerve.zeroGyro()));
+      .onTrue(new InstantCommand(() -> swerve.zeroGyro())); */
+    new JoystickButton(primary, XboxController.Button.kRightBumper.value)
+      .onTrue(new InstantCommand(() -> elevator.setState(ElevatorState.MID)));
+    new JoystickButton(primary, XboxController.Button.kLeftBumper.value)
+      .onTrue(new InstantCommand(() -> elevator.setState(ElevatorState.DOWN)));
+    
+    // Algae manipulator controls
+    new JoystickButton(primary, XboxController.Button.kA.value)
+      .onTrue(new InstantCommand(() -> algaeManipulator.setState(AlgaeManipState.INTAKE)))
+      .onFalse(new InstantCommand(() -> algaeManipulator.setState(AlgaeManipState.IDLE)));
+    
+    new JoystickButton(primary, XboxController.Button.kX.value)
+      .onTrue(new InstantCommand(() -> algaeManipulator.setState(AlgaeManipState.RELEASE)))
+      .onFalse(new InstantCommand(() -> algaeManipulator.setState(AlgaeManipState.IDLE)));
+    
+    new JoystickButton(primary, XboxController.Button.kStart.value)
+      .onTrue(new InstantCommand(() -> algaeManipulator.setState(AlgaeManipAngleState.OUT)));
+    
+    new JoystickButton(primary, XboxController.Button.kBack.value)
+      .onTrue(new InstantCommand(() -> algaeManipulator.setState(AlgaeManipAngleState.UP)));
   }
 
+  /*
   public Command getAutonomousCommand() {
     return autoModeSelector.getAutonomousCommand();
-  }
+  } */
 }
